4c522329eaf6dc175890d041b15b6809fd60ae9e
TomasGruning <tomasgruning@gmail.com>
2023-05-01 17:24:37 -0300

    correccion de la lista de 'errores'

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let palabra_elegida;
 let letras_usadas = []; 
-let errores_cont = 0, aciertos_cont = 1;
+let errores_cont = 0, aciertos_cont = 0;
+let acerto = false;
 
 const palabras = [
   'manzana',
@@ -12,41 +13,47 @@ const palabras = [
   'naranja'
 ];
 
-const letrasErradas = document.getElementById('errores');
 
 const atril = document.getElementById('imagen');
 
 //boton 'generar palabra'
 const reiniciar = document.getElementById('jugar');
-iniciar();reiniciar.addEventListener('click', iniciar);
+reiniciar.addEventListener('click', iniciar);
 
 function iniciar(event){ 
   atril.src = 'assets/img0.png';
   reiniciar.disabled = true;
   errores_cont = 0; aciertos_cont = 0;
-
+  
+  //crea los spans para los errores
+  const letrasErradas = document.getElementById('errores');
+  letrasErradas.innerHTML = '';
+  for(let x=0; x < 7; x++) {
+    letrasErradas.appendChild(document.createElement('span'));
+  }
+  
   const parrafo = document.getElementById('palabra_a_adivinar');
   parrafo.innerHTML = '';
-  letrasErradas.innerHTML = '';
-  document.getElementById('resultado').innerHTML = '';
-
+  
   //elije una palabra aleatoria
   palabra_elegida = palabras[Math.floor(Math.random() * palabras.length)];
-  console.log(palabra_elegida);
+  console.log(palabra_elegida); 
 
   //crea lo guiones para cada letra de la palabra
   for(let x=0; x < palabra_elegida.length; x++) {
     parrafo.appendChild(document.createElement('span'));
   }
-
+  
   document.addEventListener('keydown', teclaPresionada);
+  console.log(palabras);
 }
 
-let acerto = false;
+
 
 function teclaPresionada(event) {
   acerto = false;
   const spans_palabra = document.querySelectorAll('#palabra_a_adivinar span');
+  const spans_errores = document.querySelectorAll('#errores span');
 
   //si la letra esta en la palabra
   let tecla = event.key.toLowerCase(); 
@@ -59,25 +66,21 @@ function teclaPresionada(event) {
   }
 
   //si la letra no esta en la palabra
-  const spans_errores = document.querySelectorAll('#errores span');
-  if(acerto == false && tecla.match(/^[a-z]$/) && letras_usadas.indexOf(tecla) == (-1)) {
-    letras_usadas.push(tecla);
-    letrasErradas.appendChild(document.createElement('span'));
+  if(acerto == false && tecla.match(/^[a-z]$/) && letras_usadas.indexOf(tecla) == -1) {
     spans_errores[errores_cont].innerHTML = tecla.toUpperCase();
-    console.log(letras_usadas[errores_cont] + " : " + errores_cont);
+    
+    letras_usadas.push(tecla);
     errores_cont++;
-
+    
     atril.src = "assets/img" + errores_cont + ".png";
   }
 
   if(errores_cont == 7) {
-    document.getElementById('resultado').innerHTML = "Perdiste, la palabra era " + palabra_elegida;
     document.removeEventListener('keydown', teclaPresionada);
     reiniciar.disabled = false;
   }
   else if(aciertos_cont == palabra_elegida.length) {
     atril.src = "assets/win.png";
-    document.getElementById('resultado').innerHTML = "Ganaste !";
     document.removeEventListener('keydown', teclaPresionada);
     reiniciar.disabled = false;
   }
